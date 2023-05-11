e9b8c749fb037afef9ce6b29aed330416e8cf7b0
Alexander Moshkov <9aq360zqmuf1@mail.ru>
2023-05-11 11:36:51 +0500

    add some code

---
+++ main.py
@@ -0,0 +1,36 @@
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqUtils import CodonUsage, gc_fraction
+
+
+def get_nucleotides_frequency(seq: Seq) -> dict[str, int]:
+    total_count = len(seq)
+    return {
+        'A': seq.count('A') / total_count,
+        'G': seq.count('G') / total_count,
+        'C': seq.count('C') / total_count,
+        'T': seq.count('T') / total_count,
+    }
+
+
+def get_rna_seq(dna_seq: Seq) -> Seq:
+    return dna_seq.transcribe()
+
+
+def get_protein_seq(rna_seq: Seq) -> Seq:
+    return rna_seq.translate()
+
+
+def main():
+    seq = next(SeqIO.parse('covid_sequence.fasta', "fasta")).seq
+
+    print("Nucleotides frequency: ", get_nucleotides_frequency(seq))
+    print("GC: ", gc_fraction(seq))
+
+    rna_seq = get_rna_seq(seq)
+    print("RNA seq: ", rna_seq)
+    print("Protein seq: ", get_protein_seq(rna_seq))
+
+
+if __name__ == '__main__':
+    main()

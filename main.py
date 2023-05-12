2b381da35d96026c6f02496303cd6771b3aa31c2
Alexander Moshkov <9aq360zqmuf1@mail.ru>
2023-05-12 21:02:35 +0500

    add codon frequency function

--- main.py
+++ main.py
@@ -21,6 +21,20 @@ def get_protein_seq(rna_seq: Seq) -> Seq:
     return rna_seq.translate()
 
 
+def get_codon_frequency(dna_seq: Seq) -> dict[str, int]:
+    codon_freq = dict()
+    for i in range(0, len(dna_seq), 3):
+        codon = dna_seq[i:i+3]
+        if codon not in codon_freq:
+            codon_freq[codon] = 0
+        codon_freq[codon] += 1
+
+    total_count = len(dna_seq) // 3
+    for codon, count in codon_freq.items():
+        codon_freq[codon] = count / total_count
+    return codon_freq
+
+
 def main():
     seq = next(SeqIO.parse('covid_sequence.fasta', "fasta")).seq
 
@@ -29,7 +43,10 @@ def main():
 
     rna_seq = get_rna_seq(seq)
     print("RNA seq: ", rna_seq)
-    print("Protein seq: ", get_protein_seq(rna_seq))
+    protein_seq = get_protein_seq(rna_seq)
+    print("Protein seq: ", protein_seq)
+
+    print("Codon freq: \n", get_codon_frequency(seq))
 
 
 if __name__ == '__main__':
